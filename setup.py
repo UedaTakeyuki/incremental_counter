4055e84780b06c6e612460e50bc8505c6e7ce657
Dr. Takeyuki Ueda <ueda@latelierdueda.com>
2018-11-05 18:45:41 +0900

    version 0.1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as f:
 setup(
   name = 'incremental_counter',
   packages = ['incremental_counter'], # this must be the same as the name above
-  version = '0.1.1',
+  version = '0.1.2',
   description = 'Incremental Counter',
   long_description=long_description,
   long_description_content_type="text/markdown",
